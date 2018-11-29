0bb7ce16d05f2874a632c6f13abec42cde3cff70
Eric Rector <eric.m.rector@gmail.com>
2018-11-29 15:12:11 +0100

    Added

---
+++ web/content/index.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <title>Hello...</title>
+  <meta charset="utf-8"> 
+</head>
+<body>
+    <div class="container">
+		<?php echo "<h1>Hi! I'm happy</h1>"; ?>
+    </div>
+</body>
+</html>
